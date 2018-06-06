dce94050e5ae861badf355e4c43c92f48dc12413
HELLO! <18256993189@163.com>
2018-06-06 14:09:09 +0800

    Delete TestingUI.py

--- TestingUI.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'TestingUI.ui'
-#
-# Created: Wed Jun 06 12:55:46 2018
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
